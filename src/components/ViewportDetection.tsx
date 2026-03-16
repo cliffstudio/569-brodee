ffd1058a8f434949c8e36c500bbc01e456a37548
Jodie <jodie@cliff.studio>
2026-03-16 13:14:54 +0000

    Update animation timing in ViewportDetection component
    - Decreased animation duration from 2000ms to 1500ms for quicker transitions.
    - Adjusted delay increment from 0.4s to 0.2s to enhance the stagger effect for elements entering the viewport.

--- src/components/ViewportDetection.tsx
+++ src/components/ViewportDetection.tsx
@@ -28,9 +28,9 @@ export default function ViewportDetection() {
           ;(batch as HTMLElement[]).forEach((el, index) => {
             const isOutOfView = el.classList.contains('out-of-view')
             const animationName = isOutOfView ? 'fadeInUp' : 'fadeIn'
-            const delay = 0.4 + index * 0.4
+            const delay = 0.2 + index * 0.2
 
-            el.style.animation = `${animationName} 2000ms cubic-bezier(0.19, 1, 0.22, 1) ${delay}s both`
+            el.style.animation = `${animationName} 1500ms cubic-bezier(0.19, 1, 0.22, 1) ${delay}s both`
           })
         },
       })
